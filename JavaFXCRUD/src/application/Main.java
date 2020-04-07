5dc709178fc6679e09282c95f6e195723c5794e5
lauslim12 <nicholasdwiarto@yahoo.com>
2020-04-07 10:31:35 +0700

    Added features to the main function.

--- JavaFXCRUD/src/application/Main.java
+++ JavaFXCRUD/src/application/Main.java
@@ -1,18 +1,38 @@
 package application;
 
+// Natural Java Libraries
 import java.io.IOException;
 
+// JavaFX Libraries
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
+// Model, View, Controller
+import model.*;
+import view.*;
+
 public class Main extends Application {
 	
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+	
+	public Main() {
+		// Sample data.
+		personData.add(new Person("Nicholas", "Python, Ruby"));
+		personData.add(new Person("Attar", "Java, C"));
+		personData.add(new Person("Sinzie", "PHP, HTML"));
+	}
+	
+	public ObservableList<Person> getPersonData() {
+		return personData;
+	}
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -50,6 +70,9 @@ public class Main extends Application {
 			
 			// Set to center.
 			rootLayout.setCenter(personOverview);
+			
+			PersonOverviewController controller = loader.getController();
+			controller.setMainApp(this);
 		}
 		catch (IOException e) {
 			e.printStackTrace();
